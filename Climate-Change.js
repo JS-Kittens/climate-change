8e98a5acae5f7755b37d63a90dcc27c028d05043
AMNWolfDog <30536793+AMNWolfDog@users.noreply.github.com>
2017-08-02 15:17:46 -0600

    Update Climate-Change.js

---
+++ Climate-Change.js
@@ -0,0 +1,36 @@
+var page=0;
+
+function beginStory() {
+  window.open('climate-story.html', "Save the Animals!", "height=600,width=737,directories=no,location=no,resizable=yes,menubar=no,toolbar=no,scrollbars=yes");
+}
+
+function nextA() {
+  debugger;
+
+    if (page===0){
+      document.getElementById("storywords").innerHTML="sdgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdogdog dgo dgod go dog do godf gofd godfg odf godf gofd god ogodfgo fdo gofd godf god fogodfg odfog odf gofdog ofdg odfo gfod gofdogofdog";
+
+      document.getElementById("storypic").src="Yoosung.PNG";
+
+      document.getElementById("choiceA").innerHTML="test2";
+      document.getElementById("choiceB").innerHTML="test2";
+    }
+    if (page===1){
+      document.getElementById("storywords").innerHTML="dog";
+
+      document.getElementById("choiceA").innerHTML="test2";
+      document.getElementById("choiceB").innerHTML="test2";
+    }
+
+  page++;
+}
+
+  function nextB() {
+    debugger;
+
+    if (page===0){
+      document.getElementById("storywords").innerHTML="mouse";
+    }
+
+  page++;
+}
